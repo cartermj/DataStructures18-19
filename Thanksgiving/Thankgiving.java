23ec600e8fd8dd33084cbd4e8d41abcb98abbd5b
cartermj20 <cartermj20@BFHS-378270D.pwcs.edu>
2018-11-19 12:30:19 -0500

    rgsrg

--- Thanksgiving/Thankgiving.java
+++ Thanksgiving/Thankgiving.java
@@ -1,4 +1,19 @@
-
-public class Thankgiving {
-
+import java.io.File;
+public class Thankgiving
+{
+    private Map< String<Integer>, Integer > foods = new TreeMap<>;
+    public static void scanMethod()
+    {
+        Scanner scan = null;
+        try
+        {
+            File f = new File("wrlghw0ripg")
+            scan = new Scanner(f);
+            
+            while ( scan = hasNext() )
+            {
+                
+            }
+        }
+    }
 }
